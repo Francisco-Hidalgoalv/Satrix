e7a57c834e193ac7647f3451235382614db142f7
FranHidal <220300010@ucaribe.edu.mx>
2025-04-18 11:26:17 -0500

    Estuve a dos de matarme

--- satrix/src/app/app.component.ts
+++ satrix/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router'; // 👈 Agrega RouterModule
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterModule, CommonModule], // 👈 RouterModule aquí
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'satrix';
--- satrix/src/app/app.routes.ts
+++ satrix/src/app/app.routes.ts
@@ -1,6 +1,29 @@
 import { Routes } from '@angular/router';
-import { InicioComponent } from './pages/inicio/inicio.component';
 
 export const routes: Routes = [
-  { path: '', component: InicioComponent }
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/inicio/inicio.component').then(m => m.InicioComponent),
+  },
+  {
+    path: 'aviso',
+    loadComponent: () =>
+      import('./pages/aviso/aviso.component').then(m => m.AvisoComponent),
+  },
+  {
+    path: 'preguntas',
+    loadComponent: () =>
+      import('./pages/preguntas/preguntas.component').then(m => m.PreguntasComponent),
+  },
+  {
+    path: 'contacto',
+    loadComponent: () =>
+      import('./pages/contacto/contacto.component').then(m => m.ContactoComponent),
+  },
+  {
+    path: 'conecta',
+    loadComponent: () =>
+      import('./pages/conecta/conecta.component').then(m => m.ConectaComponent),
+  },
 ];
